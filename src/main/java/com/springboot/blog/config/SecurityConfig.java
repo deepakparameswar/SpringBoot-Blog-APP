72c7c54bf030eba5a25a484779ab05a92437a1cc
Deepak K <koravangattu.deepak@gmail.com>
2025-11-29 11:18:27 +0530

    login-singup API Developement

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable).
                 authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
